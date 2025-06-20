90dbf4973f7b4c4fee8a0bfe2a81f04f4e80d11e
janithcmw <janithcmw@gmail.com>
2025-06-20 21:17:46 +0530

    add location for resource module.

--- Infrastructure/Terraform/resouce-group.tf
+++ Infrastructure/Terraform/resouce-group.tf
@@ -5,9 +5,10 @@ provider "azurerm" {
   features {}
 }
 
-# Creating resouce group.
-module "resouce_group" {
+# Creating resource group.
+module "resource_group" {
     source                      = "github.com/wso2/azure-terraform-modules//modules/azurerm/Resource-Group?ref=v0.44.0"
     resource_group_name         = local.resource_group_name
+    location                    = var.location
     tags                        = var.tags
 }
