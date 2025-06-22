56b3f41572022a91d301ba019b01df26e100a9e8
janithcmw <janithcmw@gmail.com>
2025-06-23 00:20:22 +0530

    complete comment out of storage account and complete bastion configs.

--- Infrastructure/Terraform/bastion.tf
+++ Infrastructure/Terraform/bastion.tf
@@ -26,10 +26,10 @@ module "bastion_vm" {
   resource_group_name       = module.resource_group.resource_group_name
   location                  = var.location
   size                      = var.bastion_vm_size
-  admin_username            = ""
-  computer_name             = ""
+  admin_username            = var.bastion_admin_username
+  computer_name             = var.bastion_computer_name
   source_image_id           = "Canonical:0001-com-ubuntu-server-focal:20_04-lts:latest"
-  public_key_path           = "./ssh/id_rsa.pub"
+  public_key_path           = var.bastion_public_ssh_key_path
   os_disk_name              = join("-", [var.bastion_vm_name, "disk"])
   os_disk_size_gb           = 30
   nic_name                  = "bastion_vm_nic"
--- Infrastructure/Terraform/storage_account.tf
+++ Infrastructure/Terraform/storage_account.tf
@@ -1,4 +1,4 @@
 # Create storage account.
-module "common_storage_account" {
+#module "common_storage_account" {
   # TODO | hope it is better if we can change the storage account without using already created 'tfstatestoragecst'
-}
+#}
