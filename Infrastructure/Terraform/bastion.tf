9bcc99740cc00dbd70eb84029c0cd46112b5a52f
janithcmw <janithcmw@gmail.com>
2025-06-23 00:58:24 +0530

    add missing parameters in bastion.tf.

--- Infrastructure/Terraform/bastion.tf
+++ Infrastructure/Terraform/bastion.tf
@@ -1,8 +1,8 @@
 # Adding bastion for both Aks clusters.
 module "bastion" {
   source = "github.com/wso2/azure-terraform-modules//modules/azurerm/Bastion-Host?ref=v0.44.0"
-  bastion_host_abbreviation     = cluster1_aks
-  bastion_host_name             = bastion
+  bastion_host_abbreviation     = "cluster1_aks"
+  bastion_host_name             = "bastion"
   resource_group_name           = module.resource_group.resource_group_name
   location                      = var.location
   file_copy_enabled             = true
@@ -16,8 +16,11 @@ module "bastion" {
   virtual_network_name          = module.cluster1_virtual_network.virtual_network_name
   subnet_address_prefixes       = "10.1.2.0/24"
   allow_https_internet_inbound  = true
-  public_ip_abbreviation        = cluster1_aks_ip
-  public_ip_name                = bastion
+  public_ip_abbreviation        = "cluster1_aks_ip"
+  public_ip_name                = "bastion"
+  public_address_prefixes       = ["203.0.113.45/32", "198.51.100.0/28"]
+  network_security_group_name   = "bastion_host_nsg"
+
 }
 
 module "bastion_vm" {
