cdce6d8df738f9372f374d45e2fdf1b02e7b0df0
janithcmw <janithcmw@gmail.com>
2025-06-23 20:17:28 +0530

    change subnet name of bastion host to 'AzureBastionSubnet' and drop the subnet used for VM.

--- Infrastructure/Terraform/bastion.tf
+++ Infrastructure/Terraform/bastion.tf
@@ -12,7 +12,7 @@ module "bastion" {
   tunneling_enabled             = false
   sku                           = "Standard"
   tags                          = var.tags
-  bastion_subnet_name           = "cluster1_subnet_for_bastion"
+  bastion_subnet_name           = "AzureBastionSubnet"
   virtual_network_name          = module.cluster1_virtual_network.virtual_network_name
   subnet_address_prefixes       = "10.1.2.0/24"
   allow_https_internet_inbound  = true
