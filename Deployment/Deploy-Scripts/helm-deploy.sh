15568dbbfac088446e3cb7e5e63f2c90ef6e6983
janithcmw <janithcmw@gmail.com>
2025-06-24 10:42:10 +0530

    integrate scripts with azure pipeline.

---
+++ Deployment/Deploy-Scripts/helm-deploy.sh
@@ -0,0 +1,27 @@
+#navigate to helm repository location.
+cd $(Agent.BuildDirectory)/s/helm-deployment-build/
+echo "Existing files in the Helm build location." && ls
+
+#login into cluster1
+az aks get-credentials --resource-group rg-multi-dc-cst-env-westeurope --name aks-multi-dc-cst-env-westeurope-001 --overwrite-existing --admin
+sudo kubelogin convert-kubeconfig -l azurecli
+
+#create secret to access the docker repository.
+kubectl create secret docker-registry janithcmw-secret --docker-username=janithcmw --docker-password=dckr_pat_e67k076xwws2VucQVtc3oCNCSvQ --docker-email=janithcmw@gmail.com
+
+#install dc-1
+helm install apim-321-multi-dc-aks ./apim-321-fully-distributed-multi-dc/dc-1
+kubectl get pods --namespace default
+
+#login into cluster2
+az aks get-credentials --resource-group rg-multi-dc-cst-env-westeurope --name aks-multi-dc-cst-env-westeurope-002 --overwrite-existing --admin
+sudo kubelogin convert-kubeconfig -l azurecli
+
+#create secret to access the docker repository.
+kubectl create secret docker-registry janithcmw-secret --docker-username=janithcmw --docker-password=dckr_pat_e67k076xwws2VucQVtc3oCNCSvQ --docker-email=janithcmw@gmail.com
+
+#install dc-2
+helm install apim-321-multi-dc-aks ./apim-321-fully-distributed-multi-dc/dc-2
+kubectl get pods --namespace default
+
+echo "Deployment executed successfully!!!"
