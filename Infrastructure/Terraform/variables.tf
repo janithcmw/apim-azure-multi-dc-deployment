4a1a3beba84a73d4e5cb4497b133c3e793b89d4e
janithcmw <janithcmw@gmail.com>
2025-06-25 15:35:49 +0530

    fix the role issue at the Azure level and adhere to the change.

--- Infrastructure/Terraform/variables.tf
+++ Infrastructure/Terraform/variables.tf
@@ -339,8 +339,3 @@ variable "acr_resource_group" {
   description = "The ACR that store the images."
   type = string
 }
-
-#tmp variable
-variable "temp_shared_vnet_id" {
-  description = "remove this after resolving the role issue of reading cross subscriptions."
-}
