6f0a43610a07406a382f14ab811f6238cb57226e
janithcmw <janithcmw@gmail.com>
2025-06-26 08:15:04 +0530

    add ingres nginx.

--- Deployment/Deploy-Scripts/helm-deploy.sh
+++ Deployment/Deploy-Scripts/helm-deploy.sh
@@ -9,7 +9,12 @@ sudo kubelogin convert-kubeconfig -l azurecli
 #create secret to access the docker repository.
 kubectl create secret docker-registry janithcmw-secret --docker-username=janithcmw --docker-password=dckr_pat_e67k076xwws2VucQVtc3oCNCSvQ --docker-email=janithcmw@gmail.com
 
-#install dc-1
+#install in dc-1
+#install nginx in dc-1
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace -f ./apim-321-fully-distributed-multi-dc/dc-1/ingress/apim-321-multi-dc-aks-am-internal-ingress-controller.yaml
+kubectl apply -f ./apim-321-fully-distributed-multi-dc/dc-1/ingress/certificate/apim-321-multi-dc-aks-am-ingress-cert.yaml
+
+#install cluster in dc-1
 helm install apim-321-multi-dc-aks ./apim-321-fully-distributed-multi-dc/dc-1
 kubectl get pods --namespace default
 
@@ -21,6 +26,11 @@ sudo kubelogin convert-kubeconfig -l azurecli
 kubectl create secret docker-registry janithcmw-secret --docker-username=janithcmw --docker-password=dckr_pat_e67k076xwws2VucQVtc3oCNCSvQ --docker-email=janithcmw@gmail.com
 
 #install dc-2
+#install nginx in dc-2
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace -f ./apim-321-fully-distributed-multi-dc/dc-2/ingress/apim-321-multi-dc-aks-am-internal-ingress-controller.yaml
+kubectl apply -f ./apim-321-fully-distributed-multi-dc/dc-2/ingress/certificate/apim-321-multi-dc-aks-am-ingress-cert.yaml
+
+#install cluster in dc-2
 helm install apim-321-multi-dc-aks ./apim-321-fully-distributed-multi-dc/dc-2
 kubectl get pods --namespace default
 
