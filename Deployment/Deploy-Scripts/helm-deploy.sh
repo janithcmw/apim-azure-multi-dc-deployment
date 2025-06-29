075dbb905f18c351479780cf51f4b03288180653
janithcmw <janithcmw@gmail.com>
2025-06-29 19:04:31 +0530

    passing vnet values using variables.

--- Deployment/Deploy-Scripts/helm-deploy.sh
+++ Deployment/Deploy-Scripts/helm-deploy.sh
@@ -2,8 +2,6 @@
 echo "The script 'helm-deploy.sh' will be executed inside the path: " && pwd
 echo "Existing files in the Helm build location." && tree
 
-#TODO need to use the env variable and pass the image details during the installation.
-
 #Building image details based on 'helm_override_value_string'
 echo "Incoming 'helm_override_value_string' is: $helm_override_value_string"
 imageRepo=$(echo "$helm_override_value_string" | cut -d'=' -f2)
@@ -14,7 +12,7 @@ echo "Captured 'imageTag' is: $imageTag"
 #Handing resource names.
 resourceGroup=$(echo "rg-$customer_project-cst-multi-dc-westeurope")
 aksCluster_1=$(echo "aks-$customer_project-cst-multi-dc-westeurope-001")
-aksCluster_2=$(echo "aks-$customer_project-cst-multi-dc-westeurope-001")
+aksCluster_2=$(echo "aks-$customer_project-cst-multi-dc-westeurope-002")
 
 
 #login into cluster1
@@ -23,11 +21,19 @@ sudo kubelogin convert-kubeconfig -l azurecli
 
 #install in dc-1
 #install nginx in dc-1
-helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace -f ./apim-321-fully-distributed-multi-dc/dc-1/ingress/apim-321-multi-dc-aks-am-internal-ingress-controller.yaml
+helm upgrade --install ingress-nginx ingress-nginx \
+      --repo https://kubernetes.github.io/ingress-nginx \
+      --namespace ingress-nginx \
+      --create-namespace \
+      -f ./apim-321-fully-distributed-multi-dc/dc-1/ingress/apim-321-multi-dc-aks-am-internal-ingress-controller.yaml \
+      --set-string controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal-subnet"="snet-vnet-$customer_project-cst-multi-dc-westeurope-001-loadbalancer"
 kubectl apply -f ./apim-321-fully-distributed-multi-dc/dc-1/ingress/certificate/apim-321-multi-dc-aks-am-ingress-cert.yaml
 
 #install cluster in dc-1
-helm install apim-321-multi-dc-aks ./apim-321-fully-distributed-multi-dc/dc-1 --set wso2.deployment.am.image.repository="$imageRepo" --set wso2.deployment.am.image.tag="$imageTag"
+helm install apim-321-multi-dc-aks ./apim-321-fully-distributed-multi-dc/dc-1 \
+      --set wso2.deployment.am.image.repository="$imageRepo" \
+      --set wso2.deployment.am.image.tag="$imageTag" \
+      --set-string wso2.deployment.services.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal-subnet"="snet-vnet-$customer_project-cst-multi-dc-westeurope-001-nodepool"
 kubectl get pods --namespace default
 
 #login into cluster2
@@ -36,11 +42,19 @@ sudo kubelogin convert-kubeconfig -l azurecli
 
 #install dc-2
 #install nginx in dc-2
-helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace -f ./apim-321-fully-distributed-multi-dc/dc-2/ingress/apim-321-multi-dc-aks-am-internal-ingress-controller.yaml
+helm upgrade --install ingress-nginx ingress-nginx \
+      --repo https://kubernetes.github.io/ingress-nginx \
+      --namespace ingress-nginx \
+      --create-namespace \
+      -f ./apim-321-fully-distributed-multi-dc/dc-2/ingress/apim-321-multi-dc-aks-am-internal-ingress-controller.yaml \
+      --set-string controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal-subnet"="snet-vnet-$customer_project-cst-multi-dc-westeurope-002-loadbalancer"
 kubectl apply -f ./apim-321-fully-distributed-multi-dc/dc-2/ingress/certificate/apim-321-multi-dc-aks-am-ingress-cert.yaml
 
 #install cluster in dc-2
-helm install apim-321-multi-dc-aks ./apim-321-fully-distributed-multi-dc/dc-2 --set wso2.deployment.am.image.repository="$imageRepo" --set wso2.deployment.am.image.tag="$imageTag"
+helm install apim-321-multi-dc-aks ./apim-321-fully-distributed-multi-dc/dc-2 \
+      --set wso2.deployment.am.image.repository="$imageRepo" \
+      --set wso2.deployment.am.image.tag="$imageTag" \
+      --set-string wso2.deployment.services.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal-subnet"="snet-vnet-$customer_project-cst-multi-dc-westeurope-002-nodepool"
 kubectl get pods --namespace default
 
 echo "Deployment executed successfully!!!"
