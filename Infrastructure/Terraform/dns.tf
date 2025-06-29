fe645ffe0201038e3092646f244af23b6c49e46c
janithcmw <janithcmw@gmail.com>
2025-06-29 13:16:37 +0530

    add all dns link configurations.

--- Infrastructure/Terraform/dns.tf
+++ Infrastructure/Terraform/dns.tf
@@ -1,5 +1,6 @@
 # This is used to handle the DNS and Vnet mappings.
-data "azurerm_resources" "cluster_1_pvt_dns_zones" {
+#Fetch DNS zone that is created inside the aks_pool_cluster1 based on the resource group name.
+data "azurerm_resources" "cluster1_pvt_dns_zones" {
   resource_group_name = join("-", ["rg", var.project, var.application_name, var.environment, var.location, var.cluster1_padding])
   type                = "Microsoft.Network/privateDnsZones"
   depends_on = [
@@ -7,12 +8,28 @@ data "azurerm_resources" "cluster_1_pvt_dns_zones" {
   ]
 }
 
+#Define data block to get the DNS details of the fetched DNS zone in above of aks_pool_cluster1
 data "azurerm_private_dns_zone" "cluster1_aks_private_dns_zone" {
   name                = local.cluster1_aks_private_dns_zone_name
   resource_group_name = join("-", ["rg", var.project, var.application_name, var.environment, var.location, var.cluster1_padding])
 }
 
-# Link cluster2_virtual_network to DNS zone of cluster1
+#Fetch DNS zone that is created inside the aks_pool_cluster2 based on the resource group name.
+data "azurerm_resources" "cluster2_pvt_dns_zones" {
+  resource_group_name = join("-", ["rg", var.project, var.application_name, var.environment, var.location, var.cluster2_padding])
+  type                = "Microsoft.Network/privateDnsZones"
+  depends_on = [
+    module.aks_cluster_2
+  ]
+}
+
+#Define data block to get the DNS details of the fetched DNS zone in above of aks_pool_cluster2
+data "azurerm_private_dns_zone" "cluster2_aks_private_dns_zone" {
+  name                = local.cluster2_aks_private_dns_zone_name
+  resource_group_name = join("-", ["rg", var.project, var.application_name, var.environment, var.location, var.cluster2_padding])
+}
+
+# Link DNS zone of cluster1 to cluster2_virtual_network
 module "cluster1_aks_dns_cluster2_vnet_link" {
   source                              = "github.com/wso2/azure-terraform-modules//modules/azurerm/Private-DNS-Zone-Vnet-Link?ref=v0.44.0"
   private_dns_zone_vnet_link_name     = "cluster1_aks_dns_to_cluster2_vnet_link"
@@ -23,5 +40,45 @@ module "cluster1_aks_dns_cluster2_vnet_link" {
   depends_on = [
     module.aks_cluster_1,
     module.cluster2_virtual_network
+  ]
+}
+
+# Link DNS zone of cluster2 to cluster1_virtual_network to
+module "cluster2_aks_dns_cluster1_vnet_link" {
+  source                              = "github.com/wso2/azure-terraform-modules//modules/azurerm/Private-DNS-Zone-Vnet-Link?ref=v0.44.0"
+  private_dns_zone_vnet_link_name     = "cluster1_aks_dns_to_cluster2_vnet_link"
+  resource_group_name                 = join("-", ["rg", var.project, var.application_name, var.environment, var.location, var.cluster2_padding])
+  private_dns_zone_name               = data.azurerm_private_dns_zone.cluster2_aks_private_dns_zone.name
+  virtual_network_id                  = module.cluster1_virtual_network.virtual_network_id
+  #registration_enabled                = true #not used in module, have to check why?
+  depends_on = [
+    module.aks_cluster_1,
+    module.cluster2_virtual_network
+  ]
+}
+
+# Link DNS zone of cluster1 to shared_virtual_network
+module "cluster1_aks_dns_shared_vnet_link" {
+  source                              = "github.com/wso2/azure-terraform-modules//modules/azurerm/Private-DNS-Zone-Vnet-Link?ref=v0.44.0"
+  private_dns_zone_vnet_link_name     = "cluster1_aks_dns_to_shared_vnet_link"
+  resource_group_name                 = join("-", ["rg", var.project, var.application_name, var.environment, var.location, var.cluster1_padding])
+  private_dns_zone_name               = data.azurerm_private_dns_zone.cluster1_aks_private_dns_zone.name
+  virtual_network_id                  = data.azurerm_virtual_network.shared_vnet.id
+  #registration_enabled                = true #not used in module, have to check why?
+  depends_on = [
+    module.aks_cluster_1
+  ]
+}
+
+# Link DNS zone of cluster2 to shared_virtual_network
+module "cluster2_aks_dns_shared_vnet_link" {
+  source                              = "github.com/wso2/azure-terraform-modules//modules/azurerm/Private-DNS-Zone-Vnet-Link?ref=v0.44.0"
+  private_dns_zone_vnet_link_name     = "cluster1_aks_dns_to_shared_vnet_link"
+  resource_group_name                 = join("-", ["rg", var.project, var.application_name, var.environment, var.location, var.cluster2_padding])
+  private_dns_zone_name               = data.azurerm_private_dns_zone.cluster2_aks_private_dns_zone.name
+  virtual_network_id                  = data.azurerm_virtual_network.shared_vnet.id
+  #registration_enabled                = true #not used in module, have to check why?
+  depends_on = [
+    module.aks_cluster_1
   ]
 }
