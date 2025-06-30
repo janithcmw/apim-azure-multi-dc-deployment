7b4d47e70f31d05a488172da54d9fc5e333ef984
janithcmw <janithcmw@gmail.com>
2025-06-30 23:29:29 +0530

    update script to use 'aksclustercommonsakey'.

--- Deployment/Deploy-Scripts/pre-deploy.sh
+++ Deployment/Deploy-Scripts/pre-deploy.sh
@@ -10,38 +10,28 @@ az account set --subscription "$subscription_id"
 az storage account create --name aksclustercommonsa --resource-group "$resourceGroup" --location westeurope \
       --sku Standard_LRS --kind StorageV2 --access-tier Hot
 
-# share for synapse-configs
-az storage share create \
-  --account-name aksclustercommonsa \
-  --name synapse-configs \
-  --quota 2
-
-# share for executionplans
-az storage share create \
-  --account-name aksclustercommonsa \
-  --name executionplans \
-  --quota 2
-
-# share for eventpublishers
-az storage share create \
-  --account-name aksclustercommonsa \
-  --name eventpublishers \
-  --quota 2
-
-# share for eventreceivers
-az storage share create \
-  --account-name aksclustercommonsa \
-  --name eventreceivers \
-  --quota 2
-
-# share for eventstreams
-az storage share create \
-  --account-name aksclustercommonsa \
-  --name eventstreams \
-  --quota 2
-
 aksclustercommonsakey=$(az storage account keys list --resource-group "$resourceGroup" \
                               --account-name aksclustercommonsa --query "[0].value" --output tsv)
+
+# share for synapse-configs
+az storage share create --account-name aksclustercommonsa --name synapse-configs --quota 2 \
+      --account-key "$aksclustercommonsakey"
+
+# share for executionplans
+az storage share create --account-name aksclustercommonsa --name executionplans --quota 2 \
+      --account-key "$aksclustercommonsakey"
+
+# share for eventpublishers
+az storage share create --account-name aksclustercommonsa --name eventpublishers --quota 2 \
+      --account-key "$aksclustercommonsakey"
+
+# share for eventreceivers
+az storage share create --account-name aksclustercommonsa --name eventreceivers --quota 2 \
+      --account-key "$aksclustercommonsakey"
+
+# share for eventstreams
+az storage share create --account-name aksclustercommonsa --name eventstreams --quota 2 \
+      --account-key "$aksclustercommonsakey"
 
 #login into cluster1
 az aks get-credentials --resource-group "$resourceGroup" --name "$aksCluster_1" --overwrite-existing --admin
