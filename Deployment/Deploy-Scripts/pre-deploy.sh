9a52f24dfecf7d91834207f50738e8d1f89bfeaf
janithcmw <janithcmw@gmail.com>
2025-06-30 13:42:03 +0530

    support common storage location with storage account.

--- Deployment/Deploy-Scripts/pre-deploy.sh
+++ Deployment/Deploy-Scripts/pre-deploy.sh
@@ -1,4 +1,60 @@
 #Any executions prior to start deploying the Helm resources.
 echo "There is plenty of room to add logic here.."
 echo "Helm resources to be deployed..." && tree
-echo "Just Echo the variables. $subscription_id, $subscription_id_shared"
+echo "Just Echo the variables. $subscription_id, $customer_project"
+
+resourceGroup=$(echo "rg-$customer_project-cst-multi-dc-westeurope")
+
+#Create Azure File share to use between two AKS clusters.
+az account set --subscription "$subscription_id"
+az storage account create --name aksClusterCommonSA --resource-group "$resourceGroup" --location westeurope \
+      --sku Standard_LRS --kind StorageV2 --access-tier Hot
+
+# share for synapse-configs
+az storage share create \
+  --account-name aksClusterCommonSA \
+  --name synapse-configs \
+  --quota 2
+
+# share for executionplans
+az storage share create \
+  --account-name aksClusterCommonSA \
+  --name executionplans \
+  --quota 2
+
+# share for eventpublishers
+az storage share create \
+  --account-name aksClusterCommonSA \
+  --name eventpublishers \
+  --quota 2
+
+# share for eventreceivers
+az storage share create \
+  --account-name aksClusterCommonSA \
+  --name eventreceivers \
+  --quota 2
+
+# share for eventstreams
+az storage share create \
+  --account-name aksClusterCommonSA \
+  --name eventstreams \
+  --quota 2
+
+aksClusterCommonSAKey=$(az storage account keys list --resource-group "$resourceGroup" \
+                              --account-name aksClusterCommonSA --query "[0].value" --output tsv)
+
+#login into cluster1
+az aks get-credentials --resource-group "$resourceGroup" --name "$aksCluster_1" --overwrite-existing --admin
+sudo kubelogin convert-kubeconfig -l azurecli
+
+#create azure secret inside cluster1
+kubectl create secret generic azure-secret --from-literal=azurestorageaccountname=aksClusterCommonSA \
+              --from-literal=azurestorageaccountkey="$aksClusterCommonSAKey" -n default
+
+#login into cluster2
+az aks get-credentials --resource-group "$resourceGroup" --name "$aksCluster_2" --overwrite-existing --admin
+sudo kubelogin convert-kubeconfig -l azurecli
+
+#create azure secret inside cluster2
+kubectl create secret generic azure-secret --from-literal=azurestorageaccountname=aksClusterCommonSA \
+              --from-literal=azurestorageaccountkey="$aksClusterCommonSAKey" -n default
