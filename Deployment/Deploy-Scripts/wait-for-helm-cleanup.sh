0585a364a6de770bd5259f10e4e3739dbef74a90
janithcmw <janithcmw@gmail.com>
2025-07-01 21:15:35 +0530

    fix k8s resource delete validation logic.

--- Deployment/Deploy-Scripts/wait-for-helm-cleanup.sh
+++ Deployment/Deploy-Scripts/wait-for-helm-cleanup.sh
@@ -1,19 +1,18 @@
-#!/bin/bash
+# Validate only the services and the ingresses since they are the k8s objects that interact with the LBs, Vnets and Snets
 
 NAMESPACE=${1:-default}
 
 echo "Waiting for all resources in namespace: $NAMESPACE to be deleted..."
 
-while true; do
-  REMAINING=$(kubectl api-resources --namespaced=true -o name | \
-    xargs -n1 kubectl get --no-headers -n "$NAMESPACE" --ignore-not-found 2>/dev/null | \
-    grep -v '^kubernetes ' | wc -l)
-
-  if [ "$REMAINING" -eq 0 ]; then
-    echo "All resources deleted in namespace: $NAMESPACE"
-    break
-  else
-    echo "$REMAINING resource(s) still present in $NAMESPACE. Waiting..."
-    sleep 3
-  fi
+echo "Waiting for services to terminate..."
+while [[ $(kubectl get svc -n "$NAMESPACE" --no-headers | grep -iv 'kubernetes' | wc -l) -ne 0 ]]; do
+  sleep 5
 done
+echo "All services are successfully terminated."
+
+
+echo "Waiting for Ingresses to terminate..."
+while [[ $(kubectl get ingress -n "$NAMESPACE" --no-headers | wc -l) -ne 0 ]]; do
+  sleep 5
+done
+echo "All ingresses are successfully terminated."
